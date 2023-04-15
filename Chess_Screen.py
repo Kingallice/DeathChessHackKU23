cb25d00c7cdd5b76065be25aa2371884b43c51c8
lslizoski <lslizoski32@gmail.com>
2023-04-15 05:59:51 -0500

    image testing

--- Chess_Screen.py
+++ Chess_Screen.py
@@ -13,6 +13,8 @@ window = pg.display.set_mode()
 pg.display.set_caption("DEATH CHESS")
 pg.display.update()
 pg.display.flip()
+letters = ['a','b','c','d','e','f','g','h']
+numbers = ['1','2','3','4','5','6','7','8']
 
 #black images
 background = pg.image.load("board.png")
@@ -24,17 +26,17 @@ QueenB = pg.image.load("Images/Pieces/QueenB.png")
 PawnB = pg.image.load("Images/Pieces/Pwned_Black.png")
 
 #white images
-"""
-RookW = pg.image.load("")
-BishopW = pg.image.load("")
-KingW = pg.image.load("")
-QueenW = pg.image.load("")
-"""
+
+RookW = pg.image.load("Images/Pieces/w_rook.png")
+BishopW = pg.image.load("Images/Pieces/w_bishop.png")
+KingW = pg.image.load("Images/Pieces/w_king.png")
+QueenW = pg.image.load("Images/Pieces/w_queen.png")
 KnightW = pg.image.load("Images/Pieces/Horse_Girl_White.png")
 PawnW = pg.image.load("Images/Pieces/Pwned_White.png")
 
 #highlights current square of mouse
-def highlight_square(pos):
+def highlight_square():
+    pos = pg.mouse.get_pos()
     for x in range(window.get_width()//4,1160,100):
         increase = 0
         for y in range(0,828,103):
@@ -50,15 +52,13 @@ def update_board(board):
     x = window.get_width()//4
     y = 0
     for piece in range(len(state)):
-
+        if state[piece].isnumeric():
+            x += (int(state[piece])-1)*100
         if state[piece] == '/':     #goes to next row
             y += 103.5
             x = window.get_width()//4
             continue
-        for num in range(1,9):      #skips empty squares
-            if state[piece] == num:
-                for none in range(num):
-                    x += 100
+
         #displays black pieces
         if state[piece] == 'r':
             window.blit(RookB, (x,y))
@@ -74,7 +74,6 @@ def update_board(board):
             window.blit(PawnB, (x,y))
 
         #displays white pieces
-        """
         elif state[piece] == 'R':
             window.blit(RookW, (x,y))
         elif state[piece] == 'B':
@@ -83,24 +82,21 @@ def update_board(board):
             window.blit(KingW, (x,y))
         elif state[piece] == 'Q':
             window.blit(QueenW, (x,y))
-        """
-        if state[piece] == 'P':
+        elif state[piece] == 'P':
             window.blit(PawnW, (x,y))
         elif state[piece] == 'N':
             window.blit(KnightW, (x,y))
         x += 100
 
-def move():
-    if board.isLegalMove("e2e4"):
-        board.pushMove("e2e4")
+def move(uciMove):
+    board.pushMove(uciMove)
 
-move()
+
 while True:
-    pos = pg.mouse.get_pos()
     window.fill(bg_color)
     window.blit(background, (window.get_width()/4,0))
     update_board(board)
-    highlight_square(pos)
+    highlight_square()
     pg.display.update()
     pg.display.flip()
 
@@ -109,5 +105,10 @@ while True:
             pg.quit()
             sys.exit()
         elif event.type == pg.MOUSEBUTTONUP: #event to get square/piece coordinates
-            clicked_pos = pos
-            print(pos)
+            clicked_pos = pg.mouse.get_pos()
+            for x in range(8):
+                for y in range(8):
+                    if clicked_pos < window.get_width()/4 + 800 and clicked_pos > window.get_width()/4:
+                        if clicked_pos < 1160 and clicked_pos > 0:
+                            pass
+
