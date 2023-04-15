6a719da5deb575ec7ee6e8a029b1fe7fd59432fc
lslizoski <lslizoski32@gmail.com>
2023-04-15 02:29:57 -0500

    Fixed highlighting issues (mostly)

--- Chess_Screen.py
+++ Chess_Screen.py
@@ -10,25 +10,30 @@ background = pg.image.load("board.png")
 #Checks to see if mouse is on certain squares
 def highlight_square(pos):
     for x in range(360,1160,100):
+        increase = 0
         for y in range(0,828,103):
+            increase += 0.5
             if pos[0] > x and pos[0] < x + 100:
-                if pos[1] > y and pos[1] < y + 103:
-                    square = pg.Surface((100,103),pg.SRCALPHA)
+                if pos[1] > y and pos[1] < y + 103 + increase:
+                    square = pg.Surface((100,103 + increase), pg.SRCALPHA)
                     square.fill((255,255,0,75))
                     window.blit(square,(x,y))
 
-
 while True:
-    for event in pg.event.get():  # PyGame event to quit the game
-        if event.type == pg.QUIT:
-            pg.quit()
-            sys.exit()
-
     pos = pg.mouse.get_pos()
     window.fill(bg_color)
     window.blit(background, (window.get_width()/4,0))
     highlight_square(pos)
     pg.display.update()
     pg.display.flip()
+
+    for event in pg.event.get():  #event to quit the game
+        if event.type == pg.QUIT:
+            pg.quit()
+            sys.exit()
+        elif event.type == pg.MOUSEBUTTONUP: #event to get square/piece coordinates
+            clicked_pos = pos
+            print(pos)
+
 
 
