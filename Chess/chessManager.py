e2d8db08b3093573c1254923c463653fb3d0b582
Noah <noahmosel73@gmail.com>
2023-04-14 23:53:30 -0500

    updates to chessManager

--- Chess/chessManager.py
+++ Chess/chessManager.py
@@ -2,25 +2,32 @@ import chess
 
 class ChessManager:
     def __init__(self) -> None:
+        """Initialized the ChessManager."""
         self.board = chess.Board()
 
     def getFen(self):
+        """Returns the fen of the board."""
         return self.board.fen
 
     def getTurn(self):
+        """Get current player turn of the board."""
         if (self.board.fullmove_number % 2 == 0):
             return "BLACK"
         return "WHITE"
     
     def isLegalMove(self, uciMove=None):
+        """Return True if passed move is legal."""
         if uciMove in self.board.generate_legal_moves:
             return True
         return False
     
     def pushMove(self, uciMove=None):
+        """Pushes the passed move to the board."""
         if uciMove:
             self.board.push_uci(uciMove)
             return True
         return False
-
-print(ChessManager().getTurn())
+    
+    def revertMove(self):
+        """Removes the last move from the board."""
+        return self.board.move_stack.pop()
